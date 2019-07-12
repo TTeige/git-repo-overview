1a6ade0e561b872166157ee9b2dca603e63a7f26
t155132 <tim.teige91@gmail.com>
2019-07-12 16:45:28 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,181 @@
+import argparse
+import os
+import re
+from collections import namedtuple
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class Repository:
+    def __init__(self, name, link):
+        self.name = name
+        self.link = link
+
+
+def _json_object_hook(d):
+    return namedtuple('X', d.keys())(*d.values())
+
+
+def get_names_from_file(args):
+    if args.repository_name_file is not None:
+        if args.names is None:
+            args.names = []
+        if os.path.exists(args.repository_name_file):
+            with open(args.repository_name_file, 'r') as f:
+                for l in f:
+                    args.names.append(l.strip('\n'))
+
+
+def match_regex(args, matching_repos, i):
+    if args.regex is not None:
+        match = re.search(args.regex, i.name, re.M | re.I)
+        if match:
+            matching_repos.append(i)
+
+
+def match_names(args, matching_repos, i):
+    if args.names is not None:
+        for name in args.names:
+            if name in i.name:
+                matching_repos.append(i)
+
+
+def get_bitbucket_repos(args, proxies, obj=None):
+    if obj is not None:
+        resp = requests.get(args.bitbucket_url + "/projects/FEL/repos?start=" + str(obj.nextPageStart), verify=False,
+                            auth=HTTPBasicAuth(args.b_user, args.b_pw),
+                            proxies=proxies)
+    else:
+        resp = requests.get(args.bitbucket_url + "/projects/FEL/repos", verify=False,
+                            auth=HTTPBasicAuth(args.b_user, args.b_pw),
+                            proxies=proxies)
+    return resp.json(object_hook=_json_object_hook)
+
+
+def get_filter_repos(args, matching_repos):
+    to_remove = []
+    if args.exclude is not None:
+        for r in matching_repos:
+            for e in args.exclude:
+                if e in r.name:
+                    to_remove.append(r)
+    return to_remove
+
+
+def create_checklist(args, matching_repos):
+    s = "Checklist\n"
+    for repo_list in matching_repos:
+        for r in repo_list:
+            if args.links:
+                link = ''
+                for l in r.links:
+                    for l2 in l:
+                        if not hasattr(l2, 'name'):
+                            link = l2.href
+                s += '- [ ] [' + r.name + '](' + link + ')\n'
+            else:
+                s += '- [ ] ' + r.name + '\n'
+    if args.checklist:
+        with open(args.output, 'w+') as f:
+            f.write(s)
+    if args.verbose:
+        print(s, end='')
+
+
+def handle_bitbucket(args):
+    proxies = {
+        'https': args.https_proxy,
+        'http': args.http_proxy,
+    }
+    matching_repos = []
+    obj = get_bitbucket_repos(args, proxies)
+
+    while not obj.isLastPage:
+        for i in obj.values:
+            match_regex(args, matching_repos, i)
+            match_names(args, matching_repos, i)
+
+        obj = get_bitbucket_repos(args, proxies, obj)
+
+    to_remove = get_filter_repos(args, matching_repos)
+
+    return [x for x in matching_repos if x not in to_remove]
+
+
+def input_args():
+    parser = argparse.ArgumentParser(description='Create a list of repositories pertinent to a team')
+    parser.add_argument('--bitbucket_password', '-p', dest='b_pw',
+                        default=os.environ.get("BITBUCKET_PASSWORD"),
+                        help='password for bitbucket')
+    parser.add_argument('--https_proxy', default='socks5://localhost:14122',
+                        help='proxy for https')
+    parser.add_argument('--http_proxy', default='socks5://localhost:14122',
+                        help='proxy for http')
+    parser.add_argument('--bitbucket_username', '-u', dest='b_user',
+                        default=os.environ.get("BITBUCKET_USERNAME"),
+                        help='username for bitbucket')
+    parser.add_argument('--search_regex', '-x', dest='regex',
+                        help='regex which is used to search for the repositories')
+    parser.add_argument('--repository_name_file', '-i', help='list of names to be searched for')
+    parser.add_argument('--strict_list_search', help='does the list contain qualified names?')
+    parser.add_argument('--names', '-m', nargs='*', help='names to search for')
+    parser.add_argument('--checklist', '-c', help='create a markdown checklist for the found repos',
+                        action='store_true')
+    parser.add_argument('--output', '-o', help='output file name', default='checklist.md')
+    parser.add_argument('--verbose', '-v', action='store_true')
+    parser.add_argument('--links_inline', '-l', dest='links', help='write output as links', action='store_true')
+    parser.add_argument('--bitbucket_url', help='repository base url', default='https://stash.adeo.no/rest/api/1.0')
+    parser.add_argument('--bitbucket', help='Search bitbucket for repositories', default=True, action='store_true')
+    parser.add_argument('--github', help='Search Github for repositories', default=True, action='store_true')
+    parser.add_argument('--github_login_user', help='Github username', default=os.environ.get("GITHUB_USERNAME"),
+                        dest='g_user')
+    parser.add_argument('--github_login_password', help='Github password', default=os.environ.get("GITHUB_PASSWORD"),
+                        dest='g_pw')
+    parser.add_argument('--github_org', help='Organization to search through', default='navikt')
+    parser.add_argument('--github_user', help='User to search through')
+    parser.add_argument('--exclude', help='exclude names', nargs='*')
+
+    return parser.parse_args()
+
+
+def handle_github(args):
+    query_string = ""
+    if args.github_user != "" or args.github_user is not None:
+        query_string = "/users/" + args.github_user + "/repos"
+    else:
+        query_string = "/orgs/" + args.github_org + "/repos"
+
+    resp = requests.get('https://api.github.com', auth=HTTPBasicAuth(args.g_user, args.g_pw))
+    if resp.status_code != 200:
+        return
+
+    next_page = resp.links['next']
+    last = resp.links['last']
+    current = next_page
+
+    obj = resp.json(object_hook=_json_object_hook)
+
+    for i in obj:
+        pass
+
+    while current != last:
+        resp = requests.get('https://api.github.com', auth=HTTPBasicAuth(args.g_user, args.g_pw))
+        next_page = resp.links['next']
+        last = resp.links['last']
+        current = next_page
+
+
+def main(args):
+    get_names_from_file(args)
+    matching_repos = []
+    if args.bitbucket:
+        matching_repos.append(handle_bitbucket(args))
+    elif args.github:
+        matching_repos.append(handle_github(args))
+    if args.checklist or args.verbose:
+        create_checklist(args, matching_repos)
+
+
+if __name__ == '__main__':
+    main(input_args())
